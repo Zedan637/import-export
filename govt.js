a76f93860931bcec46525445f93c8871045491b9
zedan <you@example.com>
2022-05-13 17:04:46 +0530

    govt.js

---
+++ govt.js
@@ -0,0 +1,6 @@
+let tax = 10;
+let discount = 4;
+let amount = 5;
+
+//export {tax, disocunt}
+module.exports={tax, discount, amount};
---
+++ state.js
@@ -0,0 +1,7 @@
+const {tax, discount, amount} = require('./govt');
+
+//import{tax, discount} from './govt'
+
+console.log(tax)
+console.log(discount)
+console.log(amount)
