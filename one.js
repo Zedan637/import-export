9a3e6ff89c321720064491a19e1a2194dd912832
zedan <you@example.com>
2022-05-13 17:09:39 +0530

    one

---
+++ one.js
@@ -0,0 +1,6 @@
+let tax = 18;
+let amount = 5;
+let get_Function = () => {
+    console.log("Function Executing")
+}
+module.exports= {tax, amount, get_Function}
---
+++ two.js
@@ -0,0 +1,4 @@
+const{tax, amount, get_Function} = require('./one')
+console.log(tax)
+console.log(amount)
+get_Function()
